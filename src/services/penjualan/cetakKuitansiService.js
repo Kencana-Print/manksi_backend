a0924cb72ea3075e6064aea78c45692c42f770df
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-09-11 18:22:52 +0700

    1.0.9

--- src/services/penjualan/cetakKuitansiService.js
+++ src/services/penjualan/cetakKuitansiService.js
@@ -131,6 +131,7 @@ const getById = async (nomor) => {
     `SELECT
       d.invd_spk_nomor, d.invd_jumlah, d.invd_harga,
       COALESCE(
+        NULLIF(so.so_nama2, ''),
         NULLIF(
           IF(SUBSTRING(d.invd_spk_nomor,4,2)='BI', b.brg_name, s.spk_nama2),
           ''
@@ -139,6 +140,7 @@ const getById = async (nomor) => {
       ) AS nama_barang
     FROM tinv_dtl d
     LEFT JOIN tspk s ON s.spk_nomor = d.invd_spk_nomor
+    LEFT JOIN tsalesorder so ON so.so_nomor = s.spk_so_ref
     LEFT JOIN tbarang b ON b.brg_kode = d.invd_spk_nomor
     WHERE d.invd_inv_nomor = ?
     ORDER BY d.invd_nourut`,
