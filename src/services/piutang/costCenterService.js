de042f8012d0a33c0331a87b4de4b25fb6c5a897
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-09-25 07:41:01 +0700

    cc

--- src/services/piutang/costCenterService.js
+++ src/services/piutang/costCenterService.js
@@ -2,10 +2,10 @@ const db = require("../../config/database");
 
 const getAll = async () => {
   const [headers] = await db.query(
-    `SELECT cc_kode, cc_nama, cc_cabang FROM financenew.tcostcenter ORDER BY cc_nama`,
+    `SELECT cc_kode, cc_nama, cc_cabang FROM finance.tcostcenter ORDER BY cc_nama`,
   );
   const [details] = await db.query(
-    `SELECT dc_kode, dc_nama FROM financenew.tcostcenteritem ORDER BY dc_kode, dc_nama`,
+    `SELECT dc_kode, dc_nama FROM finance.tcostcenteritem ORDER BY dc_kode, dc_nama`,
   );
   return headers.map((h) => ({
     ...h,
@@ -16,8 +16,8 @@ const getAll = async () => {
 const search = async (query) => {
   let sql = `
     SELECT cc.cc_kode, cc.cc_nama, cc.cc_cabang, dc.dc_nama
-    FROM financenew.tcostcenteritem dc
-    INNER JOIN financenew.tcostcenter cc ON cc.cc_kode = dc.dc_kode
+    FROM finance.tcostcenteritem dc
+    INNER JOIN finance.tcostcenter cc ON cc.cc_kode = dc.dc_kode
     WHERE CAST(cc.cc_kode AS UNSIGNED) >= 10
   `;
   const params = [];
