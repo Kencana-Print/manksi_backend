afd19eac63ccec8737b7020583f49d86148c94cf
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-09-26 11:00:13 +0700

    detail informasi bahan

--- src/services/tools/informasiBahanService.js
+++ src/services/tools/informasiBahanService.js
@@ -157,9 +157,10 @@ const getKartuPergerakan = async (kode, startDate = null, endDate = null) => {
         m.mst_noreferensi AS Nomor,
         DATE_FORMAT(m.mst_tanggal, '%Y-%m-%d') AS Tanggal,
         m.mst_stok_out AS Jumlah,
-        NULL AS SpkNomor,
+        hr.promin_spk_nomor AS SpkNomor,   
         'GB001' AS Gudang
       FROM tmasterstok_bahan m
+      LEFT JOIN tproduksiminta_hdr hr ON hr.promin_nomor = m.mst_noreferensi   
       WHERE m.mst_aktif = 'Y' AND m.mst_gdg_kode = 'GB001'
         AND LEFT(m.mst_noreferensi, 4) = 'PROG'
         AND m.mst_brg_kode = ? ${dateFilterReal}
